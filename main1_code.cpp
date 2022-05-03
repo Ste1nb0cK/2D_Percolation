b4bdb5ee69454b42689a8b57d72c7f8c0ea2e905
Ste1nb0cK <ste1nb0ck@ste1n>
2022-05-03 09:07:53 -0500

    Changed all the matrices to arrays

--- main1_code.cpp
+++ main1_code.cpp
@@ -9,32 +9,33 @@
  * hay que corregirlo pero por ahora tenemos esto y creo que tenemos casi lista
  * la implementación para ponernos a hacer medidas y demás.*/
 
-Eigen::Array<bool, Eigen::Dynamic, Eigen::Dynamic> fill(int L, double p,
-                                                        int seed); /*Función
-que llena el arreglo con las casillas abiertas (1) y cerradas (0).
+Eigen::ArrayXXi fill(int L, double p, int seed);
+/*Función
+que llena el arreglo con las casillas abiertas (1) y cerradas (0).*/
 
-void hood(const  Eigen::Ref<Eigen::MatrixXi> I,  Eigen::Ref<Eigen::MatrixXi> F,
-int L, int ii, int jj, int target);/*Esta función a partir de una casilla (i,
-j) de la matriz I, crea una matriz F con un clouster formado por casillas con
+void hood(const Eigen::Ref<Eigen::ArrayXXi> M, Eigen::Ref<Eigen::ArrayXXi> F,
+          int L, int ii, int jj, int target);
+/*Esta función a partir de una casilla (i,
+j) de la matriz M, crea una matriz F con un cluster formado por casillas con
 el valor de "target" y todas las demás casillas fuera del cluster particular
-son ceros. */
+son cero. */
 
-Eigen::MatrixXi
-Path(const Eigen::Ref<Eigen::MatrixXi> I, int L, int scanning,
+Eigen::ArrayXXi
+Path(const Eigen::Ref<Eigen::ArrayXXi> I, int L, int scanning,
      int target); /*Este método toma la matriz de entrada I y devuelve otra
                      matriz con todos los clusters del valor target
                      (percolantes o no) que inician en la arista izquierda si el
                      valor de scanning es 1, o desde la derecha si el valor de
                      scanning es 0. */
 
-Eigen::MatrixXi clouster_matrix(
+Eigen::ArrayXXi clouster_matrix(
     const Eigen::Ref<Eigen::MatrixXi> I,
     int L); /*Este método toma la matriz I, y nos devuelve lo que llamé"la
                matriz de percolación, que estará llena con el número 2 en todas
                las casillas que hagan parte de un clouster percolante, los demás
                valores son ceros." */
 
-bool Percolation(const Eigen::Ref<Eigen::MatrixXi> I,
+bool Percolation(const Eigen::Ref<Eigen::ArrayXXi> I,
                  int L); /*Este método recibe la matriz de percolación y nos
 dice si existe un clouster percolante o no. */
 
@@ -52,31 +53,27 @@ int main(int argc, char *argv[]) {
   const int L = std::atoi(argv[1]);
   const double p = std::atof(argv[2]);
   const double seed = std::atof(argv[3]);
-  Eigen::Array<bool, Eigen::Dynamic, Eigen::Dynamic> M;
+  Eigen::ArrayXXi M;
   M = fill(L, p, seed);
   // Eigen::MatrixXi F(L, L);
   // F = clouster_matrix(M, L);
 
-  std::cout << "La matriz original es" << std::endl
-            << M
-            << std::endl;
-      // << "La matriz de percolación es" << std::endl
-      // << F << std::endl
-      // << "¿EL sistema percola?" << std::endl
-      // << Percolation(F, L) << std::endl;
+  std::cout << "La matriz original es" << std::endl << M << std::endl;
+  // << "La matriz de percolación es" << std::endl
+  // << F << std::endl
+  // << "¿EL sistema percola?" << std::endl
+  // << Percolation(F, L) << std::endl;
 
-      return 0;
+  return 0;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
-Eigen::Array<bool, Eigen::Dynamic, Eigen::Dynamic> fill(int L, double p,
-                                                         int seed) {
+Eigen::ArrayXXi fill(int L, double p, int seed) {
 
   std::mt19937 gen(seed);
   std::bernoulli_distribution dis(p);
-  Eigen::Array<bool, Eigen::Dynamic, Eigen::Dynamic>
-      M; // Declaramos un arreglo de tamaño LxL.
+  Eigen::ArrayXXi M; // Declaramos un arreglo de tamaño LxL.
   M.resize(L, L);
   // for (int ii = 0; ii < L; ++ii) {
   // for (int jj = 0; jj < L;
@@ -105,7 +102,7 @@ Eigen::Array<bool, Eigen::Dynamic, Eigen::Dynamic> fill(int L, double p,
 
 ///////////////////////////////////////////////////////////////////////
 
-void hood(const Eigen::Ref<Eigen::MatrixXi> I, Eigen::Ref<Eigen::MatrixXi> F,
+void hood(const Eigen::Ref<Eigen::ArrayXXi> I, Eigen::Ref<Eigen::ArrayXXi> F,
           int L, int ii, int jj, int target) {
 
   if (ii >= L || ii < 0 || jj >= L || jj < 0 || I(ii, jj) != target ||
@@ -127,10 +124,10 @@ void hood(const Eigen::Ref<Eigen::MatrixXi> I, Eigen::Ref<Eigen::MatrixXi> F,
 ////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////////
-Eigen::MatrixXi Path(const Eigen::Ref<Eigen::MatrixXi> I, int L, int scanning,
+Eigen::ArrayXXi Path(const Eigen::Ref<Eigen::ArrayXXi> I, int L, int scanning,
                      int target) {
 
-  Eigen::MatrixXi F(L,
+  Eigen::ArrayXXi F(L,
                     L); /*Se crea una matriz de ceros F, la cual se llenará con
                          todos los clousters que nacen en la frontera izquierda
                          si scanning==1, para eso se usa la función hood, si
@@ -157,25 +154,25 @@ Eigen::MatrixXi Path(const Eigen::Ref<Eigen::MatrixXi> I, int L, int scanning,
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-Eigen::MatrixXi clouster_matrix(const Eigen::Ref<Eigen::MatrixXi> I, int L) {
+Eigen::ArrayXXi clouster_matrix(const Eigen::Ref<Eigen::ArrayXXi> I, int L) {
 
-  Eigen::MatrixXi F(L, L); // Creamos F llena de ceros.
+  Eigen::ArrayXXi F(L, L); // Creamos F llena de ceros.
 
   F = Path(I, L, 1, 1); // Se crean los caminos de números 1 desde la izquierda
                         // de la matriz que hay en la matriz I.
 
-  Eigen::MatrixXi H(L, L);
+  Eigen::ArrayXXi H(L, L);
   H = I.transpose(); // Declaramos H, que será la transpuesta de I, esto para
                      // identificar los clousters verticales usando el mismo alg
                      // oritmo,
 
-  Eigen::MatrixXi J(L, L);
+  Eigen::ArrayXXi J(L, L);
   J = Path(H, L, 1,
            1); // Se crean los caminos de números desde la izquerda de la
                // transpuesta de I, lo cual es equivalente a crearlos de sde la
                // parte de arriba de I.
 
-  Eigen::MatrixXi med(
+  Eigen::ArrayXXi med(
       L, L); // Para crear la matriz percolante sumamos F y la transpuesta de F,
              // las casillas que hagan parte de un clous ter, al sumarlas, el
              // resultado debe ser 2, sin embargo esto no es suficiente, porque
@@ -183,7 +180,7 @@ Eigen::MatrixXi clouster_matrix(const Eigen::Ref<Eigen::MatrixXi> I, int L) {
              // son percolantes.
   med = F + J.transpose();
 
-  Eigen::MatrixXi F1(
+  Eigen::ArrayXXi F1(
       L,
       L); // Para solucionar esto, creamos una matriz F1, que tomará la
           // matriz med y le creará los caminos buscando el número 2 pero
@@ -199,7 +196,7 @@ Eigen::MatrixXi clouster_matrix(const Eigen::Ref<Eigen::MatrixXi> I, int L) {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-bool Percolation(const Eigen::Ref<Eigen::MatrixXi> I, int L) {
+bool Percolation(const Eigen::Ref<Eigen::ArrayXXi> I, int L) {
 
   int cont =
       0; /* Esta función está pensada para que reciba una matriz de percolación
