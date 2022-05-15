81931433172114d0e295b02a5a4f34169ef2e27c
Ste1nb0cK <nicolas0nn7@gmail.com>
2022-05-15 17:18:48 -0500

    Modified main1_cope.cpp  so that only the value of percolation (1 if it percolates, 0 if it does not) and the size of the biggest percolant cluster.

--- src/main1_code.cpp
+++ src/main1_code.cpp
@@ -35,13 +35,13 @@ int main(int argc, char *argv[]) {
 
   
 
-  std::cout << "La matriz original es" << std::endl << M << std::endl
-  << "La matriz de percolación indexada es" << std::endl
-  << G << std::endl
-  << "¿EL sistema percola?" << std::endl
-  << Percolation(G, L) << std::endl
-  <<"EL tamaño del clouster percolante más grande es"<<std::endl
-	    <<Size_clouster(G, L)<<std::endl;
-
+  // std::cout << "La matriz original es" << std::endl << M << std::endl
+  // << "La matriz de percolación indexada es" << std::endl
+  // << G << std::endl
+  // << "¿EL sistema percola?" << std::endl
+  // << Percolation(G, L) << std::endl
+  // <<"EL tamaño del clouster percolante más grande es"<<std::endl
+	    // <<Size_clouster(G, L)<<std::endl;
+std::cout << Percolation(G, L) << "\t" << Size_clouster(G, L) << "\n";
   return 0;
 }
