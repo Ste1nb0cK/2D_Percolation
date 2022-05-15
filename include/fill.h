bccba7b14cf2b4754f653a1a2b07b7dffac5202c
Ste1nb0cK <nicolas0nn7@gmail.com>
2022-05-15 16:51:43 -0500

    Deleted trash files

--- include/fill.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Eigen/Dense>
-#include <random>
-/*Función
-que llena el arreglo con las casillas abiertas (1) y cerradas (0).*/
-void fill_array(Eigen::ArrayXXi& A, int L, double p, int seed);
