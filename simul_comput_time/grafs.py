00e46fc015b22779f068aff62bedcc52947e7311
fquevedo1 <fquevedo@unal.edu.co>
2022-05-16 00:26:26 -0500

    Cambios de sintaxis en los scripts en la simulación para encontrar el tiempo de computo

---
+++ simul_comput_time/grafs.py
@@ -0,0 +1,113 @@
+import matplotlib.pyplot as plt
+
+plt.figure(0)
+
+with open('time_data01.txt') as f:
+    lines = f.readlines()
+    M = [float(line.split()[0]) for line in lines]
+    t = [float(line.split()[1]) for line in lines]
+    t_err = [float(line.split()[2]) for line in lines]
+
+plt.plot(M ,t,label='-O0')
+plt.errorbar(M,t,yerr = t_err,fmt ='o')
+
+with open('time_data11.txt') as f:
+    lines = f.readlines()
+    M = [float(line.split()[0]) for line in lines]
+    t = [float(line.split()[1]) for line in lines]
+    t_err = [float(line.split()[2]) for line in lines]
+
+plt.plot(M ,t,label='-O1')
+plt.errorbar(M,t,yerr = t_err,fmt ='o')
+
+with open('time_data31.txt') as f:
+    lines = f.readlines()
+    M = [float(line.split()[0]) for line in lines]
+    t = [float(line.split()[1]) for line in lines]
+    t_err = [float(line.split()[2]) for line in lines]
+
+plt.plot(M ,t,label='-O3')
+plt.errorbar(M,t,yerr = t_err,fmt ='o')
+
+plt.title("Tiempo de computo. p=0.3")
+plt.xlabel("Tamaño de la matriz")
+plt.ylabel("Tiempo (s)")
+plt.legend()
+plt.grid()
+plt.savefig('Computing_time_p03.png')
+
+#---------------------------------------------------------------------
+
+plt.figure(1)
+
+with open('time_data02.txt') as f:
+    lines = f.readlines()
+    M = [float(line.split()[0]) for line in lines]
+    t = [float(line.split()[1]) for line in lines]
+    t_err = [float(line.split()[2]) for line in lines]
+
+plt.plot(M ,t,label='-O0')
+plt.errorbar(M,t,yerr = t_err,fmt ='o')
+
+with open('time_data12.txt') as f:
+    lines = f.readlines()
+    M = [float(line.split()[0]) for line in lines]
+    t = [float(line.split()[1]) for line in lines]
+    t_err = [float(line.split()[2]) for line in lines]
+
+plt.plot(M ,t,label='-O1')
+plt.errorbar(M,t,yerr = t_err,fmt ='o')
+
+with open('time_data32.txt') as f:
+    lines = f.readlines()
+    M = [float(line.split()[0]) for line in lines]
+    t = [float(line.split()[1]) for line in lines]
+    t_err = [float(line.split()[2]) for line in lines]
+
+plt.plot(M ,t,label='-O3')
+plt.errorbar(M,t,yerr = t_err,fmt ='o')
+
+plt.title("Tiempo de computo. p=0.6")
+plt.xlabel("Tamaño de la matriz")
+plt.ylabel("Tiempo (s)")
+plt.legend()
+plt.grid()
+plt.savefig('Computing_time_p06.png')
+
+#---------------------------------------------------------------------
+
+plt.figure(3)
+
+with open('time_data03.txt') as f:
+    lines = f.readlines()
+    M = [float(line.split()[0]) for line in lines]
+    t = [float(line.split()[1]) for line in lines]
+    t_err = [float(line.split()[2]) for line in lines]
+
+plt.plot(M ,t,label='-O0')
+plt.errorbar(M,t,yerr = t_err,fmt ='o')
+
+with open('time_data13.txt') as f:
+    lines = f.readlines()
+    M = [float(line.split()[0]) for line in lines]
+    t = [float(line.split()[1]) for line in lines]
+    t_err = [float(line.split()[2]) for line in lines]
+
+plt.plot(M ,t,label='-O1')
+plt.errorbar(M,t,yerr = t_err,fmt ='o')
+
+with open('time_data33.txt') as f:
+    lines = f.readlines()
+    M = [float(line.split()[0]) for line in lines]
+    t = [float(line.split()[1]) for line in lines]
+    t_err = [float(line.split()[2]) for line in lines]
+
+plt.plot(M ,t,label='-O3')
+plt.errorbar(M,t,yerr = t_err,fmt ='o')
+
+plt.title("Tiempo de computo. p=0.8")
+plt.xlabel("Tamaño de la matriz")
+plt.ylabel("Tiempo (s)")
+plt.legend()
+plt.grid()
+plt.savefig('Computing_time_p08.png')
---
+++ simul_comput_time/simul_data.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash 
+
+SRC_DIR=../src
+CXX=g++
+
+$CXX -fsanitize=leak -fsanitize=address -fsanitize=leak -I "../include" -O0  $SRC_DIR/percolation_routines.cpp $SRC_DIR/fill.cpp $SRC_DIR/main_chrono.cpp -o chrono
+
+for L in 2 4 8 16 32 64 128 256 512 1024; do echo -n "$L    "; ./chrono $L 0.30; done > time_data01.txt 
+for L in 2 4 8 16 32 64 128 256 512 1024; do echo -n "$L    "; ./chrono $L 0.60; done > time_data02.txt 
+for L in 2 4 8 16 32 64 128 256; do echo -n "$L    "; ./chrono $L 0.80; done > time_data03.txt
+
+$CXX -fsanitize=leak -fsanitize=address -fsanitize=leak -I "../include"  -O1  $SRC_DIR/percolation_routines.cpp $SRC_DIR/fill.cpp $SRC_DIR/main_chrono.cpp  -o chrono
+
+for L in 2 4 8 16 32 64 128 256 512 1024; do echo -n "$L    "; ./chrono $L 0.30; done > time_data11.txt 
+for L in 2 4 8 16 32 64 128 256 512 1024; do echo -n "$L    "; ./chrono $L 0.60; done > time_data12.txt 
+for L in 2 4 8 16 32 64 128 256; do echo -n "$L    "; ./chrono $L 0.80; done > time_data13.txt
+
+
+$CXX -fsanitize=leak -fsanitize=address -fsanitize=leak -I "../include" -O3 $SRC_DIR/percolation_routines.cpp $SRC_DIR/fill.cpp $SRC_DIR/main_chrono.cpp  -o chrono
+
+for L in 2 4 8 16 32 64 128 256 512 1024; do echo -n "$L    "; ./chrono $L 0.30; done > time_data31.txt 
+for L in 2 4 8 16 32 64 128 256 512 1024; do echo -n "$L    "; ./chrono $L 0.60; done > time_data32.txt 
+for L in 2 4 8 16 32 64 128 256; do echo -n "$L    "; ./chrono $L 0.80; done > time_data33.txt
+
+
+python3 grafs.py
+
+rm *.txt chrono
+
+
+
+
+
