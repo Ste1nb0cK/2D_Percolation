b6bce59a887278f276bd67226d19ae2acb0d4a0b
fquevedo1 <fquevedo@unal.edu.co>
2022-05-20 10:36:44 -0500

    unificación scripts

--- bash.sh
+++ bash.sh
@@ -3,7 +3,7 @@
 #Ciclo para poder obtener los datos del programa
 for size in 32 64 128 256 512
 do
-  for p in {0..1..0.05}
+  for p in {0..1..0.05} 0.550 0.555 0.560 0.5625 0.565 0.5675 0.570 0.575 0.560 0.565
   do
     for n in {1..10}
     do
